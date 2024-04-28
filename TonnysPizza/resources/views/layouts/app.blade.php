ff9ded90cb8ce5ccf81618a421210548f4389290
unknown <b00138240@mytudublin.ie>
2024-04-28 23:37:39 +0100

    homepage

--- TonnysPizza/resources/views/layouts/app.blade.php
+++ TonnysPizza/resources/views/layouts/app.blade.php
@@ -15,32 +15,42 @@
     <body>
         <!-- Bootstrap 5 Navbar -->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-          <div class="container-fluid">
+          <div class="container">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href=https://laravel.com/docs>Laravel Documentation</a>
+                <a class="nav-link" href="{{url('/')}}">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href=https://laracasts.com/>Laravel Video Tutorials</a>
+                <a class="nav-link" href="{{url('/customers')}}">Customers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/drivers')}}">Drivers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/pizzas')}}">Pizzas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/orders')}}">Orders</a>
               </li>
             </ul>
           </div>
         </nav>
         <div id="page-content-wrapper">
             <div class="container-fluid">
-                <div class="row">
+                <div class="row p-5">
                     <!-- Well for left side of the page (to compensate for widescreen) -->
-                    <div class="col-lg-2"></div>
+                    {{-- <div class="col-lg-2"></div> --}}
                     <!-- Slot for content from Lower Level views with sections called 'content' pulled in here -->
-                    <div class="col-lg-8"> @yield('content') </div>
+                    <div class="col-lg-12"> @yield('content') </div>
                     <!-- Well for right side of the page (to compensate for widescreen) -->
-                    <div class="col-lg-2"></div>
+                    {{-- <div class="col-lg-2"></div> --}}
                 </div>
             </div>
         </div>
         <!-- Webpack mix npm generated -->
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <script src="{{asset('js/app.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         @stack('js_scripts')
     </body>
 </html>
